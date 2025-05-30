2bbf081a1f59b7f949f7c03a1cf524accbe17d70
Gabrielgsw <ggermanow279@gmail.com>
2025-05-29 22:02:15 -0300

    initial commit

---
+++ backend/src/main/java/com/leiamais/LeiaMaisApplication.java
@@ -0,0 +1,13 @@
+package com.leiamais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeiaMaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeiaMaisApplication.class, args);
+	}
+
+}
