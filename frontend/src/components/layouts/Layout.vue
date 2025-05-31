32d6a26e87c3732c505376840e829332a10af92f
aeciojose <aeciojunior45@gmail.com>
2025-05-31 11:17:48 -0300

    [FRONTENT] Adicionando Tailwindcss e configurações iniciais

---
+++ frontend/src/components/layouts/Layout.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+    <header>
+        <div>
+            <h1>Leia+</h1>
+            <img src="" alt="Capivara" />
+        </div>
+        <div>
+            <button>Solicite já</button>
+            <button>Entrar</button>
+        </div>
+    </header>
+</template>
+
+<style scoped></style>
