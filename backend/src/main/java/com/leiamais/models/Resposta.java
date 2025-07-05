eaff5470d8dfaef3d06225bc159823bb7a165a1c
Gabrielgsw <ggermanow279@gmail.com>
2025-07-05 20:33:30 -0300

    feat: ajustes resposta e resposta service

--- backend/src/main/java/com/leiamais/models/Resposta.java
+++ backend/src/main/java/com/leiamais/models/Resposta.java
@@ -1,38 +1,55 @@
 package com.leiamais.models;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.util.UUID;
 
 @Entity
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class Resposta {
 
     @Id
     @GeneratedValue
     private UUID id;
+
     @Column(nullable = false)
     private String texto;
+
     @Column
     private float nota;
+
     @OneToOne
     @JoinColumn(name = "aluno_id", nullable = false)
     private Aluno aluno;
+
     @OneToOne
     @JoinColumn(name = "atividade_id", nullable = false)
     private Atividade atividade;
+
     @Column
     private String feedback;
 
+
+    public Resposta() {
+    }
+
+
+    public Resposta(UUID id, String texto, float nota, Aluno aluno, Atividade atividade, String feedback) {
+        this.id = id;
+        this.texto = texto;
+        this.nota = nota;
+        this.aluno = aluno;
+        this.atividade = atividade;
+        this.feedback = feedback;
+    }
+
+
+
     public UUID getId() {
         return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
     }
 
     public String getTexto() {
@@ -50,21 +67,27 @@ public class Resposta {
     public void setNota(float nota) {
         this.nota = nota;
     }
+
     public Aluno getAluno() {
         return aluno;
     }
+
     public void setAluno(Aluno aluno) {
         this.aluno = aluno;
     }
+
     public Atividade getAtividade() {
         return atividade;
     }
+
     public void setAtividade(Atividade atividade) {
         this.atividade = atividade;
     }
+
     public String getFeedback() {
         return feedback;
     }
+
     public void setFeedback(String feedback) {
         this.feedback = feedback;
     }
